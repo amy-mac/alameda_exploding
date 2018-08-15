23478022a50ea9ecbc0c373cd7b879a8427ac585
Amy MacKinnon <amy@amy-mac.com>
2018-08-15 16:12:32 -0700

    Initial working version of sports information retrieval

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
-require "Date"
-require "Holidays"
 require "sinatra"
 require "sinatra/reloader" if development?
+require "Date"
+require "Holidays"
+require "HTTParty"
+require "Nokogiri"
+require "pry" if development?
+
+# ROUTES/ACTIONS
 
 get "/" do
   @holidays = check_holidays
@@ -9,6 +14,9 @@ get "/" do
 
   erb :index
 end
+
+
+# CLASSES/METHODS
 
 FIREWORK_HOLIDAYS = [
   "Independence Day",
@@ -32,5 +40,58 @@ def check_holidays
 end
 
 def check_sports
-  #Warriors, #Giants, #A's
+  Scraper.new.check_for_games
+end
+
+class Scraper
+  attr_accessor :check_for_games
+
+  SPORTS_TEAMS = %w[Athletics Giants Warriors].freeze
+
+  def grab_page_info(team)
+    case team
+    when "Athletics"
+      HTTParty.get("https://statsapi.mlb.com/api/v1/schedule?sportId=1&gamePk=531221&hydrate=team,linescore,flags,liveLookin,review,person,stats,probablePitcher,game(content(summary,media(epg)),tickets)&useLatestGames=true&language=en")
+    when "Giants"
+      HTTParty.get("https://statsapi.mlb.com/api/v1/schedule?sportId=1&gamePk=531220&hydrate=team,linescore,flags,liveLookin,review,person,stats,probablePitcher,game(content(summary,media(epg)),tickets)&useLatestGames=true&language=en")
+    when "Warriors"
+      HTTParty.get("https://www.nba.com/.element/media/2.0/teamsites/warriors/json/schedule-2018.json")
+    end
+  end
+
+  def check_for_games
+    games = []
+
+    SPORTS_TEAMS.each do |team|
+      response = grab_page_info(team)
+
+      if team == "Warriors"
+        home_game = response["games"].detect do |game|
+          game["home"] == true && game["date"] == Date.today.strftime("%F")
+        end
+
+        games << team unless home_game.nil?
+      elsif team == "Athletics"
+        home_game = response["dates"].first["date"] == Date.today.strftime("%F") && response["dates"].first["games"].first["venue"]["name"] == "Oakland Coliseum"
+
+        if home_game
+          night_game = response["dates"].first["games"].first["dayNight"] == "night"
+          is_winner = response["dates"].first["games"].first["teams"]["home"]["isWinner"]
+        end
+
+        games << team if home_game && night_game && is_winner
+      elsif team == "Giants"
+        home_game = response["dates"].first["date"] == Date.today.strftime("%F") && response["dates"].first["games"].first["venue"]["name"] == "AT&T Park"
+
+        if home_game
+          night_game = response["dates"].first["games"].first["dayNight"] == "night"
+          is_winner = response["dates"].first["games"].first["teams"]["home"]["isWinner"]
+        end
+
+        games << team if home_game && night_game && is_winner
+      end
+    end
+
+    games
+  end
 end
