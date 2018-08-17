4367f246fd15b6a07d3a741c948d7aee1a3eac57
Amy MacKinnon <amy@amy-mac.com>
2018-08-17 09:46:54 -0700

    Add caching for the sports queries

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "sinatra"
 require "date"
 require "holidays"
 require "httparty"
+require 'iron_cache'
 require "pry" if development?
 require "sinatra/reloader" if development?
 
@@ -37,11 +38,12 @@ def check_holidays
   end
 
   # We only care about the holidays that might have explosions
-  holidays.map { |holiday| holiday[:name] }.detect { |hol| FIREWORK_HOLIDAYS.include?(hol) }
+  holidays = holidays.map { |holiday| holiday[:name] }.detect { |hol| FIREWORK_HOLIDAYS.include?(hol) }
+  holidays
 end
 
 def check_sports
-  @check_sports ||= Scraper.new.check_for_games
+  check_sports = Scraper.new.check_for_games["teams"]
 end
 
 class Scraper
@@ -49,6 +51,11 @@ class Scraper
 
   SPORTS_TEAMS = %w[Athletics Giants Warriors].freeze
   VENUE_NAMES = ["AT&T Park", "Oakland Coliseum"].freeze
+
+  def initialize
+    @ironcache = IronCache::Client.new
+    @sports_cache = @ironcache.cache("sports")
+  end
 
   def simple_date
     @date ||= Date.today.strftime("%F")
@@ -65,11 +72,11 @@ class Scraper
   def grab_page_info(team)
     case team
     when "Athletics"
-      HTTParty.get(baseball_url("531221"))
+      info = HTTParty.get(baseball_url("531221"))
     when "Giants"
-      HTTParty.get(baseball_url("531220"))
+      info = HTTParty.get(baseball_url("531220"))
     when "Warriors"
-      HTTParty.get(basketball_url)
+      info = HTTParty.get(basketball_url)
     end
   end
 
@@ -87,7 +94,9 @@ class Scraper
   end
 
   def check_for_games
-    games ||= []
+    return JSON.parse(@sports_cache.get("games").value) if @sports_cache.get("games")
+
+    games = { teams: [] }
 
     SPORTS_TEAMS.each do |team|
       puts "Checking site for #{team}"
@@ -100,12 +109,14 @@ class Scraper
           game["home"] == true && game["date"] == simple_date
         end
 
-        games << team unless home_game.nil?
+        games[:teams] << team unless home_game.nil?
       else
-        games << team if winning_baseball_game?(response)
+        games[:teams] << team if winning_baseball_game?(response)
       end
     end
 
-    games
+    # TODO: if winning game then take longer to expire
+    @sports_cache.put("games", games.to_json, :expires_in => 120)
+    JSON.parse(@sports_cache.get("games").value)
   end
 end
